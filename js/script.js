fbe667da6d98c838681866968c9a7c178a515f55
Thiago Araújo Nascimento <thiagoaraujodeveloper@gmail.com>
2025-05-14 13:32:17 -0300

    Criado interatividade do projeto
    Foi criado a lógica de programação fazendo uso da linguagem Java Script para gerar interadividade no projeto como alterar as imagens pepise e alterar o background ao clicar nas thumbs.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color) {
+    const section = document.querySelector('.section');
+    section.style.background = color;
+}
